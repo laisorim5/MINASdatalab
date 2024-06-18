dcced740c0d2449540e91a17d57ed5dc87dda923
laisorim5 <166548288+laisorim5@users.noreply.github.com>
2024-06-18 20:58:19 -0300

    feat: Create triangle_area.js

---
+++ aula2/triangle_area.js
@@ -0,0 +1,7 @@
+// Pede as informações de base e altura do triângulo
+var base = parseFloat(prompt("Digite a base do triângulo (em metros):  "));
+var altura = parseFloat(prompt("Digite a altura do triângulo (em metros): "));
+var area = (base * altura) / 2;
+
+// Imprime o resultado da área
+console.log("A área do triângulo é: " + area + " metros quadrados");
