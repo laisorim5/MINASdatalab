c9a0982b01abb325db02161644eea3717f62e1cc
laisorim5 <166548288+laisorim5@users.noreply.github.com>
2024-06-18 21:00:29 -0300

    feat: Create celsius_to_fahr.js

---
+++ aula2/celsius_to_fahr.js
@@ -0,0 +1,8 @@
+// Pergunta o valor da temperatura em celsius a ser transformada
+var celsius = parseFloat(prompt("Digite a temperatura em graus Celsius: "));
+
+// Calcula a conversão
+var fahr = (celsius * 1.8) + 32;
+
+// Imprime o resultado final
+console.log("A temperatura em Fahrenheit é: " + fahr);
